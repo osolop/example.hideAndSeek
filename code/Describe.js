1f0d8a86d06d39be8dcee006a33dd637bcf0dae7
Alexander <sanyasolop@gmail.com>
2019-03-24 21:24:40 +0100

    Add story, finalyze structure

--- code/Describe.js
+++ code/Describe.js
@@ -1,6 +1,10 @@
 module.exports.function = function describe (location) {
   var http = require('http');
   var config = require('config');
-  var response = http.getUrl("https://osolop.pythonanywhere.com/details/" + location.toLowerCase(), { format: "json", });
+  if (typeof location === 'undefined'){
+    var response = http.getUrl("https://osolop.pythonanywhere.com/details", { format: "json", });
+  } else {
+    var response = http.getUrl("https://osolop.pythonanywhere.com/details/" + location.toLowerCase(), { format: "json", });
+  }
   return response;
 }
